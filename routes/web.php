48f348680b2b2c1295df1908dff3ed342a5f0f2e
Rafael Jaques <rafael@phpit.com.br>
2024-10-15 08:51:44 -0300

    Aula 15/10 - Status request API

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,13 @@ Route::get('/', function () {
 });
 
 Route::get('/somar', function(Request $request) {
+    // Não está chegando nada pela request
+    if (count($request->all()) < 1) {
+        return response()->json([
+            'message' => 'Não há valores para somar.'
+        ], 406);
+    }
+
     $soma = array_sum($request->all());
     return response()->json([
         'message' => 'Somado com sucesso', // Opcional
