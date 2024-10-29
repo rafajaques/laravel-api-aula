01af158a6168674977e4511654aff4d17ce5eaa5
Rafael Jaques <rafael@phpit.com.br>
2024-10-29 08:48:34 -0300

    Aula 29/10 - Desativar validação Csrf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            '*'
+            // Exemplo
+            // 'api/*'
+            // 'pagina-desprotegiada/action/*'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
