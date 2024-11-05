4ff3015340a4600ac13cce3449aa2e778c198dc2
Rafael Jaques <rafael@phpit.com.br>
2024-11-05 08:59:10 -0300

    Aula 05/11 - Implementação de token JWT

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JWTAuthController;
 use App\Http\Controllers\PeopleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -33,4 +34,8 @@ Route::prefix('/people')->group(function() {
     
     Route::post('/storeInterest', [PeopleController::class, 'storeInterest']);
 
+});
+
+Route::prefix('/user')->group(function() {
+    Route::post('/register', [JWTAuthController::class, 'register']);
 });
